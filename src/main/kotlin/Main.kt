5d0b72d04cb81a176959833f64ca05d9ab6732b0
Daisy-che <daisychepkiruih@gmail.com>
2024-02-11 14:31:55 +0300

    added file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var name = "Daisy Chepkirui"
+    println(name)
+    var age = 22
+    println(age)
+    var phoneNumber = +254748064111
+    println(phoneNumber)
+}
